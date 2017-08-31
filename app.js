6c61f29574b202e43e91785e5760494fc09bebab
Zachary Morel <morelz133@gmail.com>
2017-08-31 09:19:57 -0400

    playing with VS Code quirks.

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bodyParser = require('body-parser')
 const routes = require('./routes/routes')
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(bodyParser.json())
 
 app.use(express.static('public'))
@@ -13,6 +15,7 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 
 app.use(routes)
+
 
 app.listen(3000, () => {
   console.log("We're listening on 3000!")
